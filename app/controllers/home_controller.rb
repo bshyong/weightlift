a14273b129492b5034093d63cabefac3aaa5ad40
Ben Shyong <likeachild@gmail.com>
2013-02-06 22:34:38 -0500

    temp home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,18 @@ class HomeController < ApplicationController
   end
 
   def temp_home
+
+    require 'open-uri'
+    require 'nokogiri'
+    url = "https://spreadsheets.google.com/feeds/list/0AhERslPRRahKdFl6UFZoQTh2UVdXLXBPTDlVS1dqanc/od6/public/basic"
+    doc = Nokogiri::XML(open(url))
+    doc.remove_namespaces!
+    @rows = []
+    puts doc.xpath('/feed/entry/content').length.to_s + "total elements"
+    doc.xpath('/feed/entry/content').each do |x|
+        @rows << x.children()[0].content().split(',').collect{|d| d.split(':')[1]}
+    end
+
     render layout: "no_nav"
   end
 
